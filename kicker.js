388036b945c314800635b76fe22b76447c2bb8a6
cse-t-kutsuna <cse_t_kutsuna@incrementp.co.jp>
2020-09-29 18:12:05 +0900

    feat(kicker): add error handling and baseUrl supprot.

--- kicker.js
+++ kicker.js
@@ -5,18 +5,22 @@ const parse = require('csv-parse/lib/sync'); // requiring sync module
 const rp = require('request-promise');
 
 const csvFileName = 'input.csv';
+const baseUrl = 'http://wwwdev.mapfan.com';
 const WAIT_TIME = 3000;
 
 async function main() {
+    const http = rp.defaults({ headers: { 'User-Agent': 'request' } });
+    // const http = rp;
     let csvFile = fs.readFileSync(csvFileName);
     let data = parse(csvFile);
 
     for (let row of data) {
-        const url = row[0];
+        let url = row[0];
         if (!url) continue;
 
+        url = baseUrl + url;
         process.stdout.write(url);
-        let result = await rp(url);
+        let result = await http.get(url).catch(err => console.log(err));
         console.log(`\t${!!result}`);
         await sleep(WAIT_TIME);
     }
